3dbaa4b7c721d6ada883bfd0e8efca1f2cdc7708
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-06-12 12:01:36 +0300

    navbar is collapsing when a link is clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,12 +23,21 @@ function Navbar() {
     return (
         <NavBar bg='black' variant="dark" className='py-0' expand="md" fixed="top">
             <Container fluid className='p-2'>
-                <NavBar.Toggle aria-controls="mobile-menu" className='ms-auto' />
+                <NavBar.Toggle aria-controls="mobile-menu" id="navTogg" className='ms-auto' />
                 <NavBar.Collapse id="mobile-menu" className='justify-content-center'>
                     <Nav>
                         {navLinks.map((link, index) => {
                             return (
-                                <NavLink key={index} href={link.ref} className='fs-5 my-2 my-md-0 mx-md-3'>
+                                <NavLink
+                                    key={index}
+                                    href={link.ref}
+                                    className='fs-5 my-2 my-md-0 mx-md-3'
+                                    onClick={() => {
+                                        if (window.innerWidth <= 767) {
+                                            document.getElementById("navTogg").click()
+                                        }
+                                    }}
+                                >
                                     {link.name}
                                 </NavLink>
                             )
